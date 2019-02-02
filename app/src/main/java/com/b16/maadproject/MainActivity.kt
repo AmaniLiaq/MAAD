d57c055544fa6ebe776a73d3c6174f29c483a683
mx612 <omer.mx612@gmail.com>
2019-02-02 15:40:49 -0600

    okay

--- app/src/main/java/com/b16/maadproject/MainActivity.kt
+++ app/src/main/java/com/b16/maadproject/MainActivity.kt
@@ -1,14 +1,24 @@
 package com.b16.maadproject
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.b16.maadproject.screen.contract.MainContract
+import com.b16.maadproject.screen.presenter.MainPresenter
 import dagger.android.AndroidInjection
+import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() , MainContract.IView{
+
+//    @Inject lateinit var mainPresenter: MainContract.IPresenter
+
+    @Inject lateinit var mainPresenter: MainPresenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainPresenter.onAttach(this)
+        mainPresenter.create()
     }
 }
--- app/src/main/java/com/b16/maadproject/screen/presenter/MainPresenter.kt
+++ app/src/main/java/com/b16/maadproject/screen/presenter/MainPresenter.kt
@@ -1,10 +1,31 @@
 package com.b16.maadproject.screen.presenter
 
+import android.util.Log
+import com.b16.maadproject.model.CharacterType
 import com.b16.maadproject.screen.contract.MainContract
 import com.b16.maadproject.screen.model.MainActivityModel
+import io.reactivex.schedulers.Schedulers
+import java.util.logging.Logger
+
 import javax.inject.Inject
 
-class MainPresenter @Inject constructor(private val mainActivityModel: MainActivityModel)
-    : MainContract.IPresenter{
+class MainPresenter @Inject constructor(private val mainActivityModel: MainActivityModel) : MainContract.IPresenter {
+
+    private var mainView: MainContract.IView? = null
+
+    fun onAttach(mainView: MainContract.IView) {
+        this.mainView = mainView
+    }
+
+    fun create() {
+        //make network call
+        mainActivityModel.loadCharacters(CharacterType.SIMPSONS)
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.io())
+            .subscribe({
+            Log.INFO
+            })
+
+    }
 
 }
